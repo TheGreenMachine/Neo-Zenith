d762865dd4108f77168b96e42d3b04a5733d3670
Pete Deutsch <pete.deutsch@gmail.com>
2019-01-09 20:16:13 -0600

    updated drivetrain for velocity

--- src/main/java/frc/team1816/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/team1816/robot/subsystems/Drivetrain.java
@@ -1,7 +1,9 @@
 package frc.team1816.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.VelocityMeasPeriod;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -9,10 +11,13 @@ public class Drivetrain extends Subsystem {
     private TalonSRX leftMain, leftSlaveOne, leftSlaveTwo, rightMain, rightSlaveOne, rightSlaveTwo;
 
     private double leftPower, rightPower = 0;
+    private double kP, kI, kD, kF;
 
     public Drivetrain(int leftMain, int leftSlaveOne, int leftSlaveTwo, int rightMain, int rightSlaveOne,
                       int rightSlaveTwo) {
         super();
+
+        kP = 0.146;
         this.leftMain = new TalonSRX(leftMain);
         this.leftSlaveOne = new TalonSRX(leftSlaveOne);
         this.leftSlaveTwo = new TalonSRX(leftSlaveTwo);
@@ -38,6 +43,40 @@ public class Drivetrain extends Subsystem {
 
         this.rightSlaveOne.set(ControlMode.Follower, rightMain);
         this.rightSlaveTwo.set(ControlMode.Follower, rightMain);
+
+        this.rightMain.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 10);
+        this.leftMain.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 10);
+
+        this.rightMain.configNominalOutputForward(0, 10);
+        this.rightMain.configNominalOutputReverse(0, 10);
+        this.rightMain.configPeakOutputForward(1, 10);
+        this.rightMain.configPeakOutputReverse(-1, 10);
+
+        this.leftMain.configNominalOutputForward(0, 10);
+        this.leftMain.configNominalOutputReverse(0, 10);
+        this.leftMain.configPeakOutputForward(1, 10);
+        this.leftMain.configPeakOutputReverse(-1, 10);
+
+        this.leftMain.config_kP(0, kP, 20);
+        this.leftMain.config_kI(0, kI, 20);
+        this.leftMain.config_kD(0, kD, 20);
+        this.leftMain.config_kF(0, kF, 20);
+//        this.leftMain.config_IntegralZone(0, izone, 20);
+
+        this.rightMain.config_kP(0, kP, 20);
+        this.rightMain.config_kI(0, kI, 20);
+        this.rightMain.config_kD(0, kD, 20);
+        this.rightMain.config_kF(0, kF, 20);
+//        this.rightMain.config_IntegralZone(0, izone, 20);
+
+        this.leftMain.selectProfileSlot(0,0);
+        this.rightMain.selectProfileSlot(0,0);
+
+        this.rightMain.configVelocityMeasurementPeriod(VelocityMeasPeriod.Period_20Ms,0);
+        this.leftMain.configVelocityMeasurementPeriod(VelocityMeasPeriod.Period_20Ms,0);
+
+        this.rightMain.configVelocityMeasurementWindow(8,0);
+        this.leftMain.configVelocityMeasurementWindow(8,0);
     }
 
     public void setDrivetrain(double leftPower, double rightPower){
